4a2bdb9605b8d697f51f4b9a73087069b3b98d8e
Konstantin Kolodiy <bonewell@yandex.ru>
2024-02-20 19:33:33 +0300

    Read points

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,49 @@
-use std::{env, fs::File, io::Read, process::exit};
+use std::{
+    env,
+    fs::File,
+    io::{BufRead, BufReader, Lines, Read},
+    process::exit,
+};
 
+use geo_types::Point;
 use serde_pickle as pickle;
+use wkt::TryFromWkt;
 
 pub mod reader;
 pub mod types;
 
 fn main() {
     let args = env::args().collect::<Vec<_>>();
-    if args.len() < 2 {
-        println!("Usage: pickle [filename]");
+    if args.len() < 3 {
+        println!("Usage: pickle [map] [points]");
         exit(1);
     }
 
-    let reader: Box<dyn Read> = Box::new(File::open(&args[1]).expect("filename is not specified"));
+    // let _map = read_map(&args[1]);
+
+    for line in read_points(&args[2]).flatten() {
+        let (from, to) = parse_points(&line);
+        // let paths = find_path(find_near_platform(from), find_near_platform(to));
+        println!("{:?} -> {:?}", from, to);
+    }
+}
+
+fn read_map(filename: &String) -> types::PublicTransport {
+    let reader: Box<dyn Read> = Box::new(File::open(&filename).expect("Can not open map"));
     let decoded: pickle::Value =
-        pickle::value_from_reader(reader, Default::default()).expect("Can not parse file");
-    let tranet: types::PublicTransport = (&decoded).into();
-    print!("{:#?}", tranet);
+        pickle::value_from_reader(reader, Default::default()).expect("Can not parse map");
+    (&decoded).into()
+}
+
+fn read_points(filename: &String) -> Lines<BufReader<File>> {
+    let file = File::open(filename).expect("Can not open points");
+    BufReader::new(file).lines()
+}
+
+fn parse_points(line: &String) -> (Point<f64>, Point<f64>) {
+    let raw_points: Vec<&str> = line.split(',').collect();
+    (
+        Point::try_from_wkt_str(raw_points[0]).unwrap(),
+        Point::try_from_wkt_str(raw_points[1]).unwrap(),
+    )
 }
